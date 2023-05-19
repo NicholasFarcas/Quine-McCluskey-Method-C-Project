28536b4680fdd66d64952d8833aab690c0cafcb4
NicholasFarcas <133722263+NicholasFarcas@users.noreply.github.com>
2023-05-19 12:31:53 -0700

    Update Quine_McCluskey.c

--- Quine_McCluskey.c
+++ Quine_McCluskey.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void BaseTenToBinary(int baseTen, int variables) {
-    int binaryArray[variables];
+///converts a base ten number to a binary/base two number
+void BaseTenToBinary(int baseTen, int variables, int outputArray[]) {
     int remainder = baseTen;
     for (int i = 0; i < variables; ++i) {
         if (remainder % 2 == 1) {
-            binaryArray[variables - 1 - i] = 1;
+        	outputArray[variables - 1 - i] = 1;
         }
         else {
-            binaryArray[variables - 1 - i] = 0;
+        	outputArray[variables - 1 - i] = 0;
         }
         remainder = remainder / 2;
     }
-
-    for (int i = 0; i < variables; ++i) {
-    	printf("%d", binaryArray[i]);
-    }
-    printf("\n");
 }
-
-
-/*void *BaseTenToBinary(int baseTen, int variables) {
-    int* binaryArray = (int*)malloc(sizeof(int) * variables);
-    int remainder = baseTen;
-    for (int i = 0; i < variables; ++i) {
-        if (remainder % 2 == 1) {
-            binaryArray[variables - 1 - i] = 1;
-        }
-        else {
-            binaryArray[variables - 1 - i] = 0;
-        }
-        remainder = remainder / 2;
-    }
-
-    return binaryArray;
-}*/
+///creates an empty array of length 'variables'
+void loadEmptyArray(int variables, int array[]) {
+	for (int i = 0; i < variables; ++i) {
+		array[i] = 0;
+	}
+}
 
 
 
 int main() {
 
-/*    for (int i = 0; i < 16; ++i) {
-        int a = BaseTenToBinary(i, 4);
-    	printf("%d = %d\n", i, a);
-    }*/
-
-	for (int i = 0; i < 16; ++i) {
-		printf("%d in binary = ", i);
-		BaseTenToBinary(i, 4);
-	}
+	int variables = 4;
+	int array[variables];
+	loadEmptyArray(variables, array);
+	
+///checks for errors by comparing the base ten to the base 2 number representation of
+///all possible 4-variable numbers
+    for (int i = 0; i < 16; ++i) {
+    	BaseTenToBinary(i, 4, array);
+    	printf("%d = ", i);
+    	for (int i = 0; i < 4; ++i) {
+    		printf("%d", array[i]);
+    	}
+    	printf("\n");
+    }
 
 
     return 0;
